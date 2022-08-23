0356186bd09b35028bd4d9ca33f56b0acf2930aa
Masoud Shadlou <87391484+mshadlou@users.noreply.github.com>
2022-08-23 15:58:42 +0100

    Add files via upload

---
+++ TissueSample2/Shared/Models/Collection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TissueSample2.Shared.Models
+{
+    [Table("collections")]
+    public class Collection
+    {
+        [Key]
+        public int c_id { get; set; }
+        
+        [Required(ErrorMessage = "Disease Term is essential")]
+        [MaxLength(500)]
+        [StringLength(500, ErrorMessage = "Disease Term is too long.")]
+        public string disease_term { get; set; } = null!;
+        
+        [Required(ErrorMessage = "Title is essential")]
+        [MaxLength(500)]
+        [StringLength(500, ErrorMessage = "Title is too long.")]
+        public string title { get; set; } = null!;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public DateTime date { get; set; } = DateTime.Now;
+
+
+        // Virtual method
+        public virtual void Edit() { }
+        public virtual void Delete() { }
+    }
+}
---
+++ TissueSample2/Shared/Models/Sample.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TissueSample2.Shared.Models
+{
+    [Table("samples")]
+    public class Sample
+    {
+        [Key]
+        public int id { get; set; }
+        
+        [Required]
+        public int c_id { get; set; }
+        
+        [Required(ErrorMessage = "Donor Count is essential")]
+        [Range(1, 2000000, ErrorMessage = "Donor Count must be greater than 0.")]
+        public int donor_count { get; set; } = 0;
+        
+        [Required(ErrorMessage = "Material Type is essential")]
+        [MaxLength(500)]
+        [StringLength(500, ErrorMessage = "Material Type is too long.")]
+        public string mat_type { get; set; } = null!;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime date { get; set; }
+
+        public Sample(int id, int c_id, int donor_count, string mat_type)
+        {
+            this.id = id;
+            this.c_id = c_id;
+            this.donor_count = donor_count;
+            this.mat_type = mat_type;
+            this.date = DateTime.Now;
+        }
+
+        // Virtual method
+        public virtual void Edit() { }
+        public virtual void Delete() { }
+
+    }
+}
